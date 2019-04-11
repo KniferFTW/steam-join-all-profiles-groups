fb72ba2b8ccfbd845b28c3682545bf5dcc7f25de
KniferFTW <qwaszx.jm18@gmail.com>
2019-04-11 23:46:02 +1000

    1
    1

---
+++ steam-join-profiles-groups.user.js
@@ -0,0 +1,51 @@
+// Original Script https://greasyfork.org/en/scripts/13424-join-all-groups-on-a-profile
+
+// ==UserScript==
+// @name         Join all groups on a profile
+// @namespace    http://teamgamerfood.com
+// @version      0.1.3
+// @description  Join all of someone's steam groups
+// @author       A5 
+// @match        *://steamcommunity.com/id/*
+// @match        *://steamcommunity.com/profiles/*
+// @require      http://code.jquery.com/jquery-2.1.4.min.js
+// @grant        none
+// ==/UserScript==
+//
+/**********************************************************************************************************/
+/* * * * * * * * * * * * * * * * * * * * * Leave the rest below alone * * * * * * * * * * * * * * * * * * */
+/**********************************************************************************************************/
+
+function JoinProfilesGroups()
+{
+	return $.ajax({
+		url: 'https://steamcommunity.com/profiles/' + g_rgProfileData.steamid + '/?xml=1',
+		data: { xml:1 },
+		type: 'GET',
+		dataType: 'xml'
+	}).done(function(xml) {
+		$(xml).find('groupID64').each(function(){
+			
+			var params = {
+				action: 'join',
+				sessionID: g_sessionID,
+			};
+
+			$.ajax({
+				url: 'https://steamcommunity.com/gid/' + $(this).text(),
+				data: params,
+				type: 'POST',
+				dataType: 'json'
+			}).done(function(data) {
+					console.log('Joined Group.');
+			}).fail(function() {
+				console.log('Error processing your request. Please try again.');
+			});
+		});
+	}).fail(function() {
+		console.log('The request failed or the group custom URL is wrong.');
+	});
+}
+
+// Start invite process
+JoinProfilesGroups();
